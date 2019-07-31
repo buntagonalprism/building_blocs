881c6db769e848de71ed582749bf6b3f03780731
Alex <alex.bunting314@gmail.com>
2019-07-31 11:21:56 +1000

    Flutter web build version

--- lib/building_blocs.dart
+++ lib/building_blocs.dart
@@ -1,8 +1,8 @@
 library building_blocs;
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter_web/widgets.dart';
 import 'dart:async';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_web_test/flutter_web_test.dart';
 
 part 'src/bloc_provider.dart';
 part 'src/base_bloc.dart';
