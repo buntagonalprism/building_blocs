de2487dd2d503b80a78b2b8e2622b059fbf3402f
Alex <alex.bunting314@gmail.com>
2019-10-23 20:07:03 +1100

    remove dependencies

--- lib/building_blocs.dart
+++ lib/building_blocs.dart
@@ -1,8 +1,6 @@
 library building_blocs;
 
 import 'package:flutter_web/widgets.dart';
-import 'dart:async';
-import 'package:flutter_web_test/flutter_web_test.dart';
 
 part 'src/bloc_provider.dart';
 part 'src/base_bloc.dart';
