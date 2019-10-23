9ebe38581b4b35b07ba7a7e11b94a09e80d90501
Alex <alex.bunting314@gmail.com>
2019-10-23 16:14:57 +1100

    Remove value stream

--- lib/building_blocs.dart
+++ lib/building_blocs.dart
@@ -6,7 +6,4 @@ import 'package:flutter_web_test/flutter_web_test.dart';
 
 part 'src/bloc_provider.dart';
 part 'src/base_bloc.dart';
-part 'src/value_stream.dart';
-part 'src/value_stream_builder.dart';
-part 'src/value_stream_testing.dart';
 part 'src/context_ready_state.dart';
